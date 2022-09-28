c5572718d1d17923b62ca8dc6358e47b42881cea
scriptor.js <65604210+shadowforce78@users.noreply.github.com>
2022-09-28 17:40:29 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#discord nitro generator code
+
+import random
+import string
+import requests
+import json
+import time
+
+def main():
+    print("Discord Nitro Generator")
+    print("Made by: SaumonDeLuxe#2960")
+    print("")
+
+    while True:
+        code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(16))
+        r = requests.get("https://discordapp.com/api/v6/entitlements/gift-codes/" + code + "?with_application=false&with_subscription_plan=true")
+        if r.status_code == 200:
+            print("Valid Nitro Code: " + code)
+            with open("Nitro Codes.txt", "a") as f:
+                f.write("Valid Nitro Code: " + code + "")
+        else:
+            print("Invalid Nitro Code: " + code)
+
+        # time.sleep(0.5)
+
+if __name__ == "__main__":
+    main()
